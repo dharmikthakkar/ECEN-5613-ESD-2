57e55d41ffd1497d1a0249d075b6fa4cff5fb111
dharmikthakkar <dhth3498@colorado.edu>
2017-03-07 21:27:07 -0700

    receive size of buffer

--- Lab3_test.c
+++ Lab3_test.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <at89c51RC2.h>
 #include <stdlib.h>
+#include <string.h>
 //#include <malloc.h>
 
 void tx_data_char(char tx_data);
@@ -35,7 +36,7 @@ void tx_data_char(char tx_data){
 }
 
 void tx_data_string(char *tx_data_ptr){
-	while(*tx_data_ptr != '0'){
+	while(*tx_data_ptr != '\n'){
 		tx_data_char(*tx_data_ptr);
 		tx_data_ptr++;
 	}
@@ -45,7 +46,7 @@ char *rx_get_string(void){
 	char rx_array[100];
 	char i=0;
 	char *rx_array_ptr=rx_array;
-	while(*(rx_array_ptr+i-1) != '0'){
+	while(*(rx_array_ptr+i-1) != '\r' & *(rx_array_ptr+i-1) != '\n'){
 		*(rx_array_ptr+i) = rx_data_char();
 		i++;
 	}
@@ -55,16 +56,21 @@ char *rx_get_string(void){
 
 void main(){
 	char my_rec_data;
+	char *user_buffer_size;
 	//char tx_string[0x1000000];
 	char *tx_string_ptr;
+	char message1[100]="Please enter the desired buffer size(in bytes) between 8 and 2400 which should be a multiple of 8\n";
 //	tx_string_ptr = malloc(0x100000);
 	TMOD = 0x20;
 	TH1 = -3;
 	SCON = 0x50;
 	TI=1;
 	TR1 = 1;
-	tx_string_ptr=rx_get_string();
+//	tx_string_ptr=rx_get_string();
 //	my_rec_data = rx_data_char();
+	tx_string_ptr = message1;
 	tx_data_string(tx_string_ptr);
+	user_buffer_size=rx_get_string();
+	
   while(1);
 }
