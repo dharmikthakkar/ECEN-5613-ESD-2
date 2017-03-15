821a6110b51ad692261b56ad26fbb8d67cca630e
dharmikthakkar <dhth3498@colorado.edu>
2017-03-15 06:17:37 -0600

    Uart echo with led blinking

---
+++ MSP432/LED_blink_UART_Echo.c
@@ -0,0 +1,163 @@
+/* --COPYRIGHT--,BSD_EX
+ * Copyright (c) 2013, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *******************************************************************************
+ *
+ *                       MSP432 CODE EXAMPLE DISCLAIMER
+ *
+ * MSP432 code examples are self-contained low-level programs that typically
+ * demonstrate a single peripheral function or device feature in a highly
+ * concise manner. For this the code may rely on the device's power-on default
+ * register values and settings such as the clock configuration and care must
+ * be taken when combining code from several examples to avoid potential side
+ * effects. Also see http://www.ti.com/tool/mspdriverlib for an API functional
+ * library & https://dev.ti.com/pinmux/ for a GUI approach to peripheral configuration.
+ *
+ * --/COPYRIGHT--*/
+//******************************************************************************
+//  MSP432P401 Demo - Timer0_A3, Toggle P1.0, CCR0 Up Mode ISR, DCO SMCLK
+//
+//  Description: Toggle P1.0 using software and TA_0 ISR. Timer0_A is
+//  configured for up mode, thus the timer overflows when TAR counts
+//  to CCR0. In this example, CCR0 is loaded with 50000.
+//  ACLK = n/a, MCLK = SMCLK = TACLK = default DCO ~3MHz
+//
+//
+//           MSP432P401x
+//         ---------------
+//     /|\|               |
+//      | |               |
+//      --|RST            |
+//        |               |
+//        |           P1.0|-->LED
+//
+//
+//   William Goh
+//   Texas Instruments Inc.
+//   June 2016 (updated) | November 2013 (created)
+//   Built with CCSv6.1, IAR, Keil, GCC
+//******************************************************************************
+#include "msp.h"
+
+void gpio_setup(){
+    // Configure GPIO
+     P1->DIR |= BIT0;
+     P1->OUT |= BIT0;
+}
+
+void timer_setup(){
+    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
+    TIMER_A0->CCTL[0] = TIMER_A_CCTLN_CCIE; // TACCR0 interrupt enabled
+    TIMER_A0->CCR[0] = 50000;
+    TIMER_A0->CTL = TIMER_A_CTL_SSEL__ACLK | // SMCLK, UP mode
+            TIMER_A_CTL_MC__UP;
+
+}
+
+void uart_setup(){
+
+    // Configure UART pins
+    P1->SEL0 |= BIT2 | BIT3;                // set 2-UART pin as secondary function
+
+    // Configure UART
+    EUSCI_A0->CTLW0 |= EUSCI_A_CTLW0_SWRST; // Put eUSCI in reset
+    EUSCI_A0->CTLW0 = EUSCI_A_CTLW0_SWRST | // Remain eUSCI in reset
+            EUSCI_B_CTLW0_SSEL__SMCLK;      // Configure eUSCI clock source for SMCLK
+    // Baud Rate calculation
+    // 12000000/(16*9600) = 78.125
+    // Fractional portion = 0.125
+    // User's Guide Table 21-4: UCBRSx = 0x10
+    // UCBRFx = int ( (78.125-78)*16) = 2
+    EUSCI_A0->BRW = 78;                     // 12000000/16/9600
+    EUSCI_A0->MCTLW = (2 << EUSCI_A_MCTLW_BRF_OFS) |
+            EUSCI_A_MCTLW_OS16;
+
+    EUSCI_A0->CTLW0 &= ~EUSCI_A_CTLW0_SWRST; // Initialize eUSCI
+    EUSCI_A0->IFG &= ~EUSCI_A_IFG_RXIFG;    // Clear eUSCI RX interrupt flag
+    EUSCI_A0->IE |= EUSCI_A_IE_RXIE;        // Enable USCI_A0 RX interrupt
+
+}
+
+
+int main(void) {
+    WDT_A->CTL = WDT_A_CTL_PW |             // Stop WDT
+            WDT_A_CTL_HOLD;
+
+
+    CS->KEY = CS_KEY_VAL;                   // Unlock CS module for register access
+    CS->CTL0 = 0;                           // Reset tuning parameters
+    CS->CTL0 = CS_CTL0_DCORSEL_3;           // Set DCO to 12MHz (nominal, center of 8-16MHz range)
+    CS->CTL1 = CS_CTL1_SELA_2 |             // Select ACLK = REFO
+            CS_CTL1_SELS_3 |                // SMCLK = DCO
+            CS_CTL1_SELM_3;                 // MCLK = DCO
+    CS->KEY = 0;                            // Lock CS module from unintended accesses
+
+    uart_setup();
+    gpio_setup();
+    SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;    // Enable sleep on exit from ISR
+
+    // Enable global interrupt
+    __enable_irq();
+
+    NVIC->ISER[0] = 1 << ((TA0_0_IRQn) & 31);
+
+
+    // Enable eUSCIA0 interrupt in NVIC module
+    NVIC->ISER[0] = 1 << ((EUSCIA0_IRQn) & 31);
+
+
+    timer_setup();
+    __sleep();
+    __no_operation();
+}
+// Timer A0_0 interrupt service routine
+
+void TA0_0_IRQHandler(void) {
+    // Clear the compare interrupt flag
+    TIMER_A0->CCTL[0] &= ~TIMER_A_CCTLN_CCIFG;
+
+    P1->OUT ^= BIT0;                        // Toggle P1.0 LED
+}
+
+
+
+// UART interrupt service routine
+void EUSCIA0_IRQHandler(void)
+{
+    if (EUSCI_A0->IFG & EUSCI_A_IFG_RXIFG)
+    {
+        // Check if the TX buffer is empty first
+        while(!(EUSCI_A0->IFG & EUSCI_A_IFG_TXIFG));
+
+        // Echo the received character back
+        EUSCI_A0->TXBUF = EUSCI_A0->RXBUF;
+    }
+}
